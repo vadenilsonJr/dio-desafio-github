3ec3dc0d63e3c39d79dab59ff1cad0fd8f4ba1c8
vadenilsonJr <vadenilsonjunior@gmail.com>
2022-05-31 11:03:16 -0300

    Adição do inicio do projeto do curso Primeiros passos com o .NET

---
+++ CURSOS REALIZADOS/primeiros passos com dotnet/revisao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                    //Cadastrar
+                        break;
+                    case "2":
+                    //Listar
+                        break;
+                    case "3":
+                    //Calcular
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                    opcaoUsuario = ObterOpcaoUsuario();     
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {    
+            Console.WriteLine();
+            Console.WriteLine("Informe a opcao desejada");
+            Console.WriteLine();
+            Console.WriteLine("1 - Cadastrar aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular media geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
