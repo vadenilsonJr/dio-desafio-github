c6221be11ba8bc5a955b3339ecc2eea9e8963b25
vadenilsonJr <vadenilsonjunior@gmail.com>
2022-06-19 22:59:27 -0300

    Desafio incial C# - GFT Start #5

---
+++ Desafio inicial C# - GFT Start #5/Desafio 1 - Média/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class program {
+
+  static void Main(string[] args) {
+         
+         
+            double A, B, C;
+            double soma;
+            double media;
+            
+            A = double.Parse(Console.ReadLine());
+            A = A * 2;
+            B = double.Parse(Console.ReadLine());
+            B = B * 3;
+            C = double.Parse(Console.ReadLine());
+            C = C * 5;
+            
+            soma = A+B+C;
+            media = soma / 10;
+            Console.WriteLine("MEDIA = " + media.ToString("F1"));
+
+        }
+}
