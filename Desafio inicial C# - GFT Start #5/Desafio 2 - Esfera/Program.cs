70a6b1c15dee5cc9067cc24b20a89835d55a4f32
vadenilsonJr <vadenilsonjunior@gmail.com>
2022-06-25 19:13:28 -0300

    Adição dos desafios

---
+++ Desafio inicial C# - GFT Start #5/Desafio 2 - Esfera/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program {
+
+  static void Main(string[] args) {
+
+    double pi, raio, volume;
+ 
+    pi = 3.14159;
+ 
+    raio = double.Parse(Console.ReadLine());
+
+    volume = pi * (4.0/3) * Math.Pow( raio , 3.0 );
+    
+    Console.WriteLine($"VOLUME = {volume.ToString("F3")}");
+  }
+
+}
---
+++ Desafio inicial C# - GFT Start #5/Desafio 3 - Polígonos regulares e simples/Program.cs
@@ -0,0 +1,10 @@
+﻿using System; 
+
+class Program {
+
+    static void Main(string[] args) { 
+
+        string[] v = Console.ReadLine().Split();
+        Console.WriteLine(int.Parse(v[0]) * int.Parse(v[1]));
+    }
+}
